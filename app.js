7bbfd501cb08ad020b1e67cb71fdb465c3bcebc3
John MOHADDES <johnmohaddes@gmail.com>
2017-07-28 18:55:50 +0430

    Form

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-function Question(question, answer, correct) {
+var m;
+
+function Question(question, answers, correct) {
     
     this.question = question;
-    this.answers = answer;
+    this.answers = answers;
     this.correct = correct;
 }
+
+init();
 
 var q1 = new Question('Who is the prime minister of Canada?', ['John Treadu', 'Adam Stone', 'Bill Gate', 'Ronald Trump'], 0);
 var q2 = new Question('How many children are approximately being born every secod globally?', ['0.1 million', '1 million', '10 million', 'Less than 10000'], 0);
@@ -15,21 +19,31 @@ var questions = [q1, q2, q3, q4, q5];
 
 var n = Math.floor(Math.random() * questions.length);
 
-                                                      
+
+document.querySelector('.btn-start').addEventListener('click', function () {
+    document.querySelector('.btn-start').style.display = 'none';
+    questions[n].displayQuestion();
+});
+
+
 Question.prototype.displayQuestion = function() {
     
-    document.getElementById("question").innerHTML = this.question;
-    
-    for (var i = 0; i < this.answers.length; i++) {
-        
-        document.getElementById("answers").innerHTML =i +': '+ this.answers(i);
-    }
+    document.getElementById("question").innerHTML = this.question;        
+    document.getElementById("ans1").innerHTML = "1" + "- " + this.answers[0];        
+    document.getElementById("ans2").innerHTML = "2" + "- " + this.answers[1];        
+    document.getElementById("ans3").innerHTML = "3" + "- " + this.answers[2];
+    document.getElementById("ans4").innerHTML = "4" + "- " + this.answers[3];
 }
 
 
-document.querySelector('.btn-start').addEventListener('click', function () {
-    questions[n].displayQuestion();
-});
+
+
+function init() {
+    
+    document.getElementById("question").innerHTML = "";
+    document.querySelector('.btn-start').style.display = 'block';
+    m = 1;   
+}
                                                       
 
 
